585dc873bea9ba17e199fbfb5ce497c3d92e63b6
Johan Boström <commit@zrx.se>
2017-12-02 10:21:37 +0100

    2017 - Day 2

---
+++ AdventOfCode/Days/Day201702.cs
@@ -0,0 +1,60 @@
+﻿// -------------------------------------------------------------------------------------------------
+//  <copyright file="Day201701.cs">
+//      © Johan Boström 2017
+//  </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace AdventOfCode.Days
+{
+    using System;
+    using System.Linq;
+
+    public class Day201702 : IDay
+    {
+        public int Year => 2017;
+        public int Day => 2;
+
+        public DayResult GetResult(string input)
+        {
+
+            var rows = input
+                .Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries); // Get all rows
+
+            var sumOne = 0;
+            var sumTwo = 0;
+
+            foreach (var row in rows)
+            {
+                var largest = 0;
+                var smallest = 9999;
+
+                var numbers = row
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse);
+
+                foreach (var num in numbers)
+                {
+                    foreach (var denum in numbers.Where(n => n < num))
+                    {
+                        if (num % denum != 0)
+                            continue;
+
+                        sumTwo += num / denum;
+
+                        break;
+                    }
+
+                    if (num > largest)
+                        largest = num;
+
+                    if (num < smallest)
+                        smallest = num;
+                }
+
+                sumOne += (largest - smallest);
+            }
+
+            return new DayResult(sumOne.ToString(), sumTwo.ToString());
+        }
+    }
+}
