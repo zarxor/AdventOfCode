290592d222a53b78582258fc3df83f482763517d
Johan Boström <commit@zrx.se>
2017-12-05 22:46:20 +0100

    2017 - Day 4

---
+++ AdventOfCode/Days/Day201704.cs
@@ -0,0 +1,42 @@
+﻿// -------------------------------------------------------------------------------------------------
+//  <copyright file="Day201703.cs">
+//      © Johan Boström 2017
+//  </copyright>
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Days
+{
+    public class Day201704 : IDay
+    {
+        public int Year => 2017;
+        public int Day => 4;
+
+        public DayResult GetResult(string input)
+        {
+            var rows = input
+                .Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries); // Get all rows
+
+            var validRows = rows.Count(IsValid);
+            var validRows2 = rows.Count(IsValid2);
+
+            return new DayResult(validRows.ToString(), validRows2.ToString());
+        }
+
+        private bool IsValid(string passphrase)
+        {
+            return passphrase.Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).GroupBy(s => s).All(g => g.Count() == 1);
+        }
+
+        private bool IsValid2(string passphrase)
+        {
+            return passphrase
+                .Trim()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => new String(s.OrderBy(c => c).ToArray()))
+                .GroupBy(s => s).All(g => g.Count() == 1);
+        }
+    }
+}
