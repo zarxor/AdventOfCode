6a84f8f408e4b132164a08ba8b146c7b7f327a01
Johan Boström <commit@zrx.se>
2021-12-02 23:07:26 +0100

    Moved to WASM

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using AdventOfCode.Models;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AdventOfCode;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+// // Add services to the container.
+// builder.Services.AddRazorPages();
+// builder.Services.AddServerSideBlazor();
 // Register all days
 builder.Services.AddSingleton<IDay[]>(p => AppDomain.CurrentDomain.GetAssemblies()
         .SelectMany(s => s.GetTypes())
@@ -14,23 +17,25 @@ builder.Services.AddSingleton<IDay[]>(p => AppDomain.CurrentDomain.GetAssemblies
         .Select(t => (IDay)Activator.CreateInstance(t))
         .ToArray());
 
-var app = builder.Build();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+await builder.Build().RunAsync();
 
-app.UseHttpsRedirection();
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
 
-app.UseStaticFiles();
+// app.UseHttpsRedirection();
 
-app.UseRouting();
+// app.UseStaticFiles();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+// app.UseRouting();
 
-app.Run();
+// app.MapBlazorHub();
+// app.MapFallbackToPage("/_Host");
+
+// app.Run();
